84dfe798d93e1a20c9b36d824c1ab1fb12f2b721
Cai <1250681712@qq.com>
2023-01-26 21:55:26 +0800

    ok

---
+++ blind75/tricks.py
@@ -0,0 +1,19 @@
+def is_all_even(nums):
+    for n in nums:
+        if n % 2 != 0:
+            return False
+    return True
+
+
+# One-liner alternative
+
+nums1 = [2, 4, 6, 8, 10]
+nums2 = [2, 4, 6, 8, 9]
+print(is_all_even(nums1))
+print(is_all_even(nums2))
+
+print(all([n % 2 == 0 for n in nums1]))
+print(all([n % 2 == 0 for n in nums2]))
+
+print(any([n % 2 == 0 for n in nums1]))
+print(any([n % 2 == 0 for n in nums2]))
