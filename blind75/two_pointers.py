36fc3e061184d2a213121650a7440d6d9911a655
Cai <1250681712@qq.com>
2023-02-14 10:36:31 +0800

    ok

--- blind75/two_pointers.py
+++ blind75/two_pointers.py
@@ -21,47 +21,23 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = []
         nums.sort()
-        # print(nums)
-
-        x = len(nums)
-
-        for i in range(x - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i, v in enumerate(nums):
+            if i > 0 and v == nums[i - 1]:
                 continue
-
             l = i + 1
-            r = x - 1
+            r = len(nums) - 1
             while l < r:
-                three_sum = nums[i] + nums[l] + nums[r]
-                if three_sum == 0:
-                    # print(i, l, r, (nums[i], nums[l], nums[r]))
-                    ans.append([nums[i], nums[l], nums[r]])
+                three_sum = v + nums[l] + nums[r]
+                if three_sum < 0:
+                    l += 1
+                elif three_sum > 0:
+                    r -= 1
+                else:
+                    ans.append([v, nums[l], nums[r]])
                     l += 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                elif three_sum < 0:
-                    l += 1
-                else:
-                    r -= 1
-
         return ans
-
-
-        # n, ans = len(nums), set()
-        # nums.sort()
-        # for i in range(n):
-        #     j, k = i + 1, n - 1
-        #     while j <= k:
-        #         s = nums[i] + nums[j] + nums[k]
-        #         if i != j and i != k and j != k and s == 0:
-        #             ans.add((nums[i], nums[j], nums[k]))
-        #             j += 1
-        #             k -= 1
-        #         elif s > 0:
-        #             k -= 1
-        #         else:
-        #             j += 1
-        # return ans
 
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
